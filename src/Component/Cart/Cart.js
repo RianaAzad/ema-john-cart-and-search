9ccd5a62b996ab003aa309dddc00aacc4c98eeb0
Riana Azad <Riana1996>
2022-10-08 12:51:29 +0600

    test commit

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
+    const {cart} =props;
+
+let total =0;
+let shipping = 0;
+for (const product of cart){
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+}
+const tax = (total * 0.01).toFixed(2);
+const grandTotal = total + shipping + parseFloat(tax);
+
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summary</h2>
                 <p>Selected Item: {props.cart.length}</p>
+                <p>Total Price: {total}</p>
+                <p>Shipping: {shipping}</p>
+                <p>Tax: {tax}</p>
+                <h5>Grand Total: {(grandTotal).toFixed(2)}</h5>
         </div>
     );
 };
